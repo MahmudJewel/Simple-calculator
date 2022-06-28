f53cc7810d49421366718277787198e3a0f246a6
villain <jmbengal.mbstu@gmail.com>
2022-06-28 22:41:58 +0600

    backend updated

--- backend/calculator/urls.py
+++ backend/calculator/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path, include
-
+from calculator.views import CalculatorViews
 urlpatterns = [
-    # path('add-user', UserCreateView.as_view(), name='add-user'),
-    # path('user-info', UserView.as_view(), name='user-info'),
-    
-    # jwt auth 
-    # path('', include('djoser.urls.jwt')),
+    path('calculate', CalculatorViews.as_view(), name='calculate'),
 ]
--- backend/calculator/views.py
+++ backend/calculator/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class CalculatorViews(APIView):
+    def get(self, request, format=None):
+        num_1=request.GET.get('num_1')
+        num_2=request.GET.get('num_2')
+        operator=request.GET.get('operator')
+        result=0
+        if num_1 is not None and num_2 is not None:
+            num_1=float(num_1)
+            num_2=float(num_2)
+            if(operator=='add'):
+                result=num_1 + num_2
+            elif(operator=='sub'):
+                result=num_1 - num_2
+            elif(operator=='mul'):
+                result=num_1 * num_2
+            elif(operator=='div'):
+                if num_2==0:
+                    result='undefined'
+                else:
+                    result=num_1 / num_2
+        
+        # print('num-1 is: ', num_1)
+        # print('num-2 is: ', num_2)
+        # print('operator is: ', operator)
+        return Response(result)
